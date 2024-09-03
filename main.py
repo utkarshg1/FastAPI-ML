3d474854ab66e04683c145ed591b926614c0fea8
Utkarsh Gaikwad <gaikwadujg@gmail.com>
2024-09-03 06:17:03 +0000

    updated main

--- main.py
+++ main.py
@@ -48,6 +48,6 @@ if __name__ == "__main__":
         host="0.0.0.0",           # Bind to all interfaces
         port=8000,                # Port number for the application
         log_level="info",         # Log level
-        workers=4,                # Number of workers (adjust based on your server)
+        workers=4,                # Number of workers 
         lifespan="on"
     )
